921e173cb1d6667760f2502cb2fcb059681d774a
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-06 19:20:08 +0530

    Step 1.11 : Added multiply test run:- "ruby string_calculator.rb"
    Tests are passed

--- string_calculator.rb
+++ string_calculator.rb
@@ -10,7 +10,10 @@ class Calculator
     digits.inject(:+)
   end
 
-  
+  def multiply
+    digits.inject(:*)
+  end
+
   private
 
   def parse_input(digits_string)
@@ -22,7 +25,13 @@ describe Calculator do
   it 'sums provided digits' do
     calc = Calculator.new('1,2,3,4,5')
     calc.sum.must_equal(15)
+
     calc1 = Calculator.new('1,2,3,4,5,5')
     calc1.sum.must_equal(20)
+  end
+
+  it 'multiplies provided digits' do
+    calc = Calculator.new('1,2,3,4,5')
+    calc.multiply.must_equal(120)
   end
 end
