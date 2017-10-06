75ab81002ceb8738cfeeb8e2543da020a266fdf4
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-06 18:39:58 +0530

    Step 1.8 : Added another assertion to test the sum is returning this time the added assertion test is failing.
    run:- "ruby string_calculator.rb"
    
    Failure:
    Calculator#test_0001_sums provided digits [string_calculator.rb:16]:
    Expected: 20
      Actual: 15
    run:- "ruby string_calculator.rb"
    
    One test is failed

--- string_calculator.rb
+++ string_calculator.rb
@@ -13,6 +13,7 @@ describe Calculator do
   it 'sums provided digits' do
     calc = Calculator.new('1,2,3,4,5')
     calc.sum.must_equal(15)
-    calc.sum.must_equal(20)
+    calc1 = Calculator.new('1,2,3,4,5,5')
+    calc1.sum.must_equal(20)
   end
 end
