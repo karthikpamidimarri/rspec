ee7eb989eefd58625936f560615d5e6398afcc09
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-07 19:17:51 +0530

    Step 1.5: describe has alias name context to bundle example with common context.

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -1,9 +1,10 @@
 
-//Example group string as an argument
-describe 'Playground' do
-     //removing the same context place grouped together
-   describe 'when there are no children'
-      //To define examples we use it 
+//Example group string as an argument 
+//Describe what we test
+describe Playground do 
+     //Bundle example with common context
+   context 'when there are no children'
+      //To define examples themselves
       it 'is quite boring place'
       it 'is empty'
    end
