dbaca5111b717acba8aa885d6f8cefbfdc8fcaad
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-07 18:22:56 +0530

    Step 1.3: Write an example group for rspec with Playground example

---
+++ spec/playground_spec.rb
@@ -0,0 +1,7 @@
+
+//Example group string as an argument
+describe 'Playground' do
+   //To define examples we use it 
+   it 'is quite boring place when there are no children'
+   it 'is empty when there are no children'
+end
