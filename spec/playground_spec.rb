f61733441517d1dcf262597a65420753ddba80ff
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-07 19:13:39 +0530

    Step 1.4: Same context identified and placed in example group

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -1,7 +1,10 @@
 
 //Example group string as an argument
 describe 'Playground' do
-   //To define examples we use it 
-   it 'is quite boring place when there are no children'
-   it 'is empty when there are no children'
+     //removing the same context place grouped together
+   describe 'when there are no children'
+      //To define examples we use it 
+      it 'is quite boring place'
+      it 'is empty'
+   end
 end
