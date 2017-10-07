16431c1dee69850fc954a8254dac3ed748976a94
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-07 19:26:58 +0530

    Step 1.6: Syntax errors are fixed after running the spec using
    bundle exec rspec
    
    need to define the class Playground

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -1,10 +1,10 @@
 
-//Example group string as an argument 
-//Describe what we test
+#Example group string as an argument 
+#Describe what we test
 describe Playground do 
-     //Bundle example with common context
-   context 'when there are no children'
-      //To define examples themselves
+     #Bundle example with common context
+   context 'when there are no children' do
+      #To define examples themselves
       it 'is quite boring place'
       it 'is empty'
    end
