9dcbef7583349c991eae74d7b4c30946e05ac16f
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-13 18:32:02 +0530

    Step 1.37: using let method created variable to access all the scenarios, using "page object pattern" If get an error we can go to that particular method instead of searching entire scenario
    Run: rspec
    
    Success

--- spec/features/create_achievement_spec.rb
+++ spec/features/create_achievement_spec.rb
@@ -3,29 +3,20 @@ require_relative '../support/new_achievement_form'
 
 #In the feature file will use featrue instead of describe
 feature 'create new achievement' do
+  let(:new_achievement_form){NewAchievementForm.new}
+
   scenario 'create new achievement with valid data' do # we use scenario instead of 'it' to specify the example
     #This is the BDD of wording
-
-    new_achievement_form = NewAchievementForm.new
     new_achievement_form.visit_page.fill_in_with(
         title: 'Read a book'
     ).submit
 
     expect(page).to have_content('Achievement has been created')
     expect(Achievement.last.title).to eq('Read a book')
-
-
-
-
   end
 
   scenario 'cannot create achievement with invalid data' do
-    visit('/')
-
-    click_on('New Achievement')
-
-    click_on('Create Achievement')
-
+    new_achievement_form.visit_page.submit
     expect(page).to have_content("can't be blank")
   end
 end
