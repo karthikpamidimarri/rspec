6018bae39a702c16bbcc1c976a8636ff38813804
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-13 19:50:56 +0530

    Factory Girl Prepare Data for our Test Step 1.42:Now instead of default title passing the title manually
    FactoryGirl.create(:achievement,title: 'Just did it')
    
    run : rspec
    
    Success

--- spec/features/achievement_page_spec.rb
+++ spec/features/achievement_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'achievement page' do
   scenario 'achievement public page' do
-    achievement = FactoryGirl.create(:achievement)  #create/build create creates record in the database  , build creates only the model in memory
+    achievement = FactoryGirl.create(:achievement, title: 'Just did it')  #create/build create creates record in the database  , build creates only the model in memory
     visit("/achievements/#{achievement.id}")
 
     expect(page).to have_content('Just did it')
