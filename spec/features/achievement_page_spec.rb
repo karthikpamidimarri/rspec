921d2dd8ea8dd5218eb726820b6414ca7185bc06
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-13 19:23:43 +0530

    Factory Girl Prepare Data for our Test Step 1.39: Now the scenario is instead of verifying title now we have to verify the description, but title would be mandatory to pass like this if we have more number of fields it's been difficult so we will use FactoryGirl Object to pass the info
    Run: rspec
    
    Success

--- spec/features/achievement_page_spec.rb
+++ spec/features/achievement_page_spec.rb
@@ -7,4 +7,11 @@ feature 'achievement page' do
 
     expect(page).to have_content('Just did it')
   end
+
+  scenario 'rendere markdown description' do
+    achievement = Achievement.create(title: 'Just did it',description: 'That *was* hard')
+    visit("/achievements/#{achievement.id}")
+
+    expect(page).to have_content('<i>was</i>')
+  end
 end
