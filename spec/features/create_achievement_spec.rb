7db022bcd0afa6d3b733727a816c4f6c989fd145
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-13 13:31:58 +0530

    Step 1.16: Created a new scenario for create a new achievement with valid data
    Run: rspec
    
    Failures:
    
      1) create new achievement create new achievement with valid data
         Failure/Error: click_on('New Achievement')
    
         Capybara::ElementNotFound:
           Unable to find visible link or button "New Achievement"

---
+++ spec/features/create_achievement_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+#In the feature file will use featrue instead of describe
+feature 'create new achievement' do
+  scenario 'create new achievement with valid data' do # we use scenario instead of 'it' to specify the example
+    #This is the BDD of wording
+    visit('/')
+    click_on('New Achievement')
+
+    fill_in('Title', with: 'Read a book')
+    fill_in('Description', with:  'Nice book !!!')
+    select('Public', from: 'Privacy')
+    check('Featured achievement')
+    attach_file('Cover image', "#{Rails.root}/spec/fixtures/cover_image.png")
+    click_on('Create Achievement')
+
+    expect(page).to have_content('Achievement has been created')
+    expect(Achievement.last.title).to eq('Read a book')
+  end
+end
