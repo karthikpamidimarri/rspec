e4386654b96aea3469ecdbd5a21e99a0560704cc
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-14 16:07:34 +0530

    Step 3.2 : Implemented example to render the template for new action
    run: bin/rspec
    
    Failures:
    
     Failures:
    
      1) AchievementsController GET new renders :new template
         Failure/Error: expect(response).to render_template(:new)
    
         NoMethodError:
           assert_template has been extracted to a gem. To continue using it,
                   add `gem 'rails-controller-testing'` to your Gemfile.

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 describe AchievementsController do
+  #GET request a verb and second word is the name of the action
+  describe "GET new" do
+    it "renders :new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
 
+    it "assigns new Achivement to @achievement"
+
+  end
 end
