54df424cad7e48f8022654673dc6557bf1622806
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-14 10:34:35 +0530

    Factory Girl Prepare Data for our Test Step 1.43:We can create Object test/dummy data using Factory Girl, we have several options 1)Sequence creation for uniqueness 2)Sub-factory creation etc....

--- spec/factories/achievements.rb
+++ spec/factories/achievements.rb
@@ -1,9 +1,14 @@
 FactoryGirl.define do
   factory :achievement do
-    title "Title"
+    sequence(:title) { |n| "Achievement #{n}"} # sequences are used to maintain uniquiness id,email...
     description "description"
     privacy Achievement.privacies[:private_access]
     featured false
     cover_image "some_file.png"
+
+    #Defining Subfactory
+    factory :public_achievement do
+      privacy Achievement.privacies[:public_access]
+    end
   end
 end
