ca5083fe69f7f90fc3234d7180b0736e4e787cd6
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-14 19:47:03 +0530

    Step 3.14 : Code Refactoring : Using let create variables for valid and invalid data
    run: bin/rspec
    
    Success

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -30,26 +30,30 @@ describe AchievementsController do
 
   describe "POST create" do
     context "valid data" do
+      let(:valid_data) {FactoryGirl.attributes_for(:public_achievement)}
+
       it "redirects to achievement#show" do
-        post :create, params: {achievement: FactoryGirl.attributes_for(:public_achievement) }
+        post :create, params: {achievement: valid_data }
         expect(response).to redirect_to(achievement_path(assigns[:achievement]))
       end
 
       it "creates new achievement in database" do
         expect {
-          post :create, params: {achievement: FactoryGirl.attributes_for(:public_achievement) }
+          post :create, params: {achievement: valid_data }
         }.to change(Achievement,:count).by(1)
       end
     end
     context "invalid data" do
+      let(:invalid_data) {FactoryGirl.attributes_for(:public_achievement,title: '')}
+
       it "renders :new template" do
-        post :create, params: {achievement: FactoryGirl.attributes_for(:public_achievement,title: '') }
+        post :create, params: {achievement: invalid_data}
         expect(response).to render_template(:new)
       end
 
       it "doesn't create new achievement in the database" do
         expect{
-          post :create, params: {achievement: FactoryGirl.attributes_for(:public_achievement,title: '') }
+          post :create, params: {achievement: invalid_data }
         }.not_to change(Achievement, :count)
       end
     end
