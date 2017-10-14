34339cd5fe74dfd6968dfa4e2dfb02f713f5c4f9
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-14 19:31:29 +0530

    Step 3.9 : Privacy issue is fixing
    run: bin/rspec
    
    Failures:
    
      1) AchievementsController POST create redirects to achievement#show
         Failure/Error: expect(response).to redirect_to(achievement_path(assigns[:achievement]))
    
           Expected response to be a redirect to <http://test.host/achievements/1> but was a redirect to <http://test.host/>.
           Expected "http://test.host/achievements/1" to be === "http://test.host/".

--- spec/factories/achievements.rb
+++ spec/factories/achievements.rb
@@ -2,13 +2,17 @@ FactoryGirl.define do
   factory :achievement do
     sequence(:title) { |n| "Achievement #{n}"} # sequences are used to maintain uniquiness id,email...
     description "description"
-    privacy Achievement.privacies[:private_access]
     featured false
     cover_image "some_file.png"
 
     #Defining Subfactory
     factory :public_achievement do
-      privacy Achievement.privacies[:public_access]
+      privacy :public_access
     end
+
+    factory :private_achievement do
+      privacy :private_access
+    end
+
   end
 end
