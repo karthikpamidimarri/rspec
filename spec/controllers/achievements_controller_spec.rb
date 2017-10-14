e04315cc629cfb98f04d7cc374ccc6079d1cc8c1
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-14 18:53:50 +0530

    Step 3.4 : displaying the achievement
    run: bin/rspec
    
    Success

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -8,7 +8,19 @@ describe AchievementsController do
       expect(response).to render_template(:new)
     end
 
-    it "assigns new Achivement to @achievement"
+    it "assigns new Achivement to @achievement" do
+      get :new
+      expect(assigns(:achievement)).to be_a_new(Achievement)
+    end
+  end
 
+  describe "GET show" do
+  let(:achievement) { FactoryGirl.create(:public_achievement)}
+    it "renders :show template" do
+      get :show, params: { id: achievement.id}
+      expect(response).to render_template(:show)
+    end
+
+    it "assigns requested achievement to @achievement"
   end
 end
