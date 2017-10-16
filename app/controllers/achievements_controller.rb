d58b04f5e319701a837246ce546d5d27b36401bc
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-16 15:17:13 +0530

    Step 3.31 : Handle update action for invalid data
    run: bin/rspec
    
    Success

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -25,6 +25,8 @@ class AchievementsController < ApplicationController
     @achievement = Achievement.find(params[:id])
     if @achievement.update_attributes(achievement_params)
       redirect_to achievement_path(@achievement)
+    else
+      render :edit
     end
   end
 
