2fab1e5fdf180c6d9db055a110ff58430476888b
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-17 12:16:24 +0530

    Step 6:Implementing authenticated user functionality to allow
    with default actions of index and show
    
    authenticated has the responsibility to perform action create,
    
    and restricting for edit, update, delete actions
    
    run : rspec spec/controllers/achievements_controller_spec.rb:50
    
    Failures:
    
      1) AchievementsController authenticated user is not the owner of the achievement GET edit redirects to achievements page
         Failure/Error: expect(response).to redirect_to(achievements_path)
    
           Expected response to be a <3XX: redirect>, but was a <200: OK>
           Response body:
         # ./spec/controllers/achievements_controller_spec.rb:154:in `block (5 levels) in <top (required)>'
    
      2) AchievementsController authenticated user is not the owner of the achievement PUT update redirects to achievements page
         Failure/Error: expect(response).to redirect_to(achievements_path)
    
           Expected response to be a redirect to <http://test.host/achievements> but was a redirect to <http://test.host/achievements/1>.
           Expected "http://test.host/achievements" to be === "http://test.host/achievements/1".
         # ./spec/controllers/achievements_controller_spec.rb:161:in `block (5 levels) in <top (required)>'

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -55,6 +55,7 @@ describe AchievementsController do
     describe "PUT update" do
       it "redirects to login page" do
         put :update, params: {id: FactoryGirl.create(:public_achievement), achievement:FactoryGirl.attributes_for(:public_achievement)}
+        expect(response).to redirect_to(new_user_session_url)
       end
     end
 
@@ -66,6 +67,112 @@ describe AchievementsController do
     end
   end
 
+  #Implementing specs for Authenticated roles
+  describe "authenticated user" do
+    #create  FactoryGirl user
+    let(:user) {FactoryGirl.create(:user)}
+    #Login with this user before each example using before method
+    before do
+      #devise helper sign_in method user sign_in
+      sign_in(user)
+    end
+
+    describe "GET index" do
+      it "renders :index template" do
+        get :index
+        expect(response).to render_template(:index)
+      end
+
+      it "assigns only public achievements to template" do
+        public_achievement = FactoryGirl.create(:public_achievement)
+        get :index
+        expect(assigns(:achievements)).to match_array([public_achievement])
+      end
+    end
+
+    describe "GET show" do
+      let(:achievement) { FactoryGirl.create(:public_achievement)}
+      it "renders :show template" do
+        get :show, params: { id: achievement}
+        expect(response).to render_template(:show)
+      end
+
+      it "assigns requested achievement to @achievement" do
+        get :show, params: { id: achievement}
+        expect(assigns(:achievement)).to eq(achievement)
+      end
+    end
+
+    #GET request a verb and second word is the name of the action
+    describe "GET new" do
+      it "renders :new template" do
+        get :new
+        expect(response).to render_template(:new)
+      end
+
+      it "assigns new Achivement to @achievement" do
+        get :new
+        expect(assigns(:achievement)).to be_a_new(Achievement)
+      end
+    end
+
+    describe "POST create" do
+      context "valid data" do
+        let(:valid_data) {FactoryGirl.attributes_for(:public_achievement)}
+
+        it "redirects to achievement#show" do
+          post :create, params: {achievement: valid_data }
+          expect(response).to redirect_to(achievement_path(assigns[:achievement]))
+        end
+
+        it "creates new achievement in database" do
+          expect {
+            post :create, params: {achievement: valid_data }
+          }.to change(Achievement,:count).by(1)
+        end
+      end
+      context "invalid data" do
+        let(:invalid_data) {FactoryGirl.attributes_for(:public_achievement,title: '')}
+
+        it "renders :new template" do
+          post :create, params: {achievement: invalid_data}
+          expect(response).to render_template(:new)
+        end
+
+        it "doesn't create new achievement in the database" do
+          expect{
+            post :create, params: {achievement: invalid_data }
+          }.not_to change(Achievement, :count)
+        end
+      end
+    end
+
+    context "is not the owner of the achievement" do
+      describe "GET edit" do
+        it "redirects to achievements page" do
+          get :edit, params: {id: FactoryGirl.create(:public_achievement)}
+          expect(response).to redirect_to(achievements_path)
+        end
+      end
+
+      describe "PUT update" do
+        it "redirects to achievements page" do
+          put :update, params: {id: FactoryGirl.create(:public_achievement), achievement:FactoryGirl.attributes_for(:public_achievement)}
+          expect(response).to redirect_to(achievements_path)
+        end
+      end
+
+      describe "DELETE destroy" do
+        it "redirects to achievements page" do
+          delete :destroy, params: {id: FactoryGirl.create(:public_achievement)}
+          expect(response).to redirect_to(achievements_path)
+        end
+      end
+    end
+
+    context "is the owner of the achievement"
+
+  end
   describe "GET edit" do
     let(:achievement) { FactoryGirl.create(:public_achievement)}
 
@@ -116,52 +223,7 @@ describe AchievementsController do
     end
   end
 
-  #GET request a verb and second word is the name of the action
-  describe "GET new" do
-    it "renders :new template" do
-      get :new
-      expect(response).to render_template(:new)
-    end
 
-    it "assigns new Achivement to @achievement" do
-      get :new
-      expect(assigns(:achievement)).to be_a_new(Achievement)
-    end
-  end
-
-
-
-
-  describe "POST create" do
-    context "valid data" do
-      let(:valid_data) {FactoryGirl.attributes_for(:public_achievement)}
-
-      it "redirects to achievement#show" do
-        post :create, params: {achievement: valid_data }
-        expect(response).to redirect_to(achievement_path(assigns[:achievement]))
-      end
-
-      it "creates new achievement in database" do
-        expect {
-          post :create, params: {achievement: valid_data }
-        }.to change(Achievement,:count).by(1)
-      end
-    end
-    context "invalid data" do
-      let(:invalid_data) {FactoryGirl.attributes_for(:public_achievement,title: '')}
-
-      it "renders :new template" do
-        post :create, params: {achievement: invalid_data}
-        expect(response).to render_template(:new)
-      end
-
-      it "doesn't create new achievement in the database" do
-        expect{
-          post :create, params: {achievement: invalid_data }
-        }.not_to change(Achievement, :count)
-      end
-    end
-  end
 
 
   describe "DELETE destroy" do
