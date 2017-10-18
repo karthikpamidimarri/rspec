a978bb89c8c5413cd3c062dc648c8a48651557fe
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-18 14:14:37 +0530

    Step 6.8: Instead of before we can use background
    background is an alias for before
    scenario is an alias for it
    feature is an alias for describe
    
    bin/rspec
    
    success

--- spec/features/create_achievement_spec.rb
+++ spec/features/create_achievement_spec.rb
@@ -8,7 +8,7 @@ feature 'create new achievement' do
   let(:login_form) {LoginForm.new}
   let(:user) {FactoryGirl.create(:user)}
 
-  before{
+  background{
     login_form.visit_page.login_as(user)
   }
 
