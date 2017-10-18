dcf0323ac0b58a6972e32dd27ff739b7e60ded79
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-18 17:39:59 +0530

    Step 7.6: Verifying the associations between the models
    bin/rspec
    
    Success

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -49,6 +49,17 @@ RSpec.describe Achievement, type: :model do
       achievement = Achievement.new(title: 'Some title', user:nil)
       expect(achievement.valid?).to be_falsey
     end
-    
+
+    it 'has belongs_to_user association' do
+      # 1 approach
+      user = FactoryGirl.create(:user)
+      achievement = FactoryGirl.create(:public_achievement, user: user)
+      expect(achievement.user).to eq(user)
+
+      # 2 approach
+
+      u = Achievement.reflect_on_association(:user)
+      expect(u.macro).to eq(:belongs_to)
+    end
   end
 end
