18d450fea645ebcb260d1792ff26a37c0d131898
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-18 15:45:38 +0530

    Step 7: Test validation for achievement spec
    bin/rspec
    
    Success

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it 'requires title' do
+=begin
+       model specs are very easy it has standard structure for testing
+
+        1) Arrange data
+        2)Make action
+        3)Assert something
+=end
+      achievement = Achievement.new(title: '')  # creating data with empty title
+      achievement.valid?  #Make an action
+
+      #Expectations
+      expect(achievement.errors[:title]).to include("can't be blank")
+      #another way of testing the same thing
+      expect(achievement.errors[:title]).not_to be_empty
+
+    end
+  end
 end
