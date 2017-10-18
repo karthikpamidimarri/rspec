6176706259107cecc1e1fd66b300d251355f3a31
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-18 14:11:30 +0530

    Step 6.6: Implement authentication to all the specs
    bin/rspec
    
    success

--- spec/features/create_achievement_spec.rb
+++ spec/features/create_achievement_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 require_relative '../support/new_achievement_form'
+require_relative '../support/login_form'
 
 #In the feature file will use featrue instead of describe
 feature 'create new achievement' do
   let(:new_achievement_form){NewAchievementForm.new}
-
+  let(:login_form) {LoginForm.new}
+  let(:user) {FactoryGirl.create(:user)}
   scenario 'create new achievement with valid data' do # we use scenario instead of 'it' to specify the example
     #This is the BDD of wording
+    login_form.visit_page.login_as(user)
     new_achievement_form.visit_page.fill_in_with(
         title: 'Read a book'
     ).submit
@@ -16,6 +19,7 @@ feature 'create new achievement' do
   end
 
   scenario 'cannot create achievement with invalid data' do
+    login_form.visit_page.login_as(user)
     new_achievement_form.visit_page.submit
     expect(page).to have_content("can't be blank")
   end
