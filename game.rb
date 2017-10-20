16c8ee37e8e2cefe8d731b3122efcb7bec535a83
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-20 13:18:14 +0530

    Step 1: describing spec to climbing Character
    Failure/Error:
      describe Character do
        describe 'climbing check skill' do
          it 'climbs successfully when roll + strength is more than difficulty'
          it 'fails climbing check when roll + strength is less than difficulty'
    
        end
      end
    
    NameError:
      uninitialized constant Character

---
+++ game.rb
@@ -0,0 +1,8 @@
+describe Character do
+  describe 'climbing check skill' do
+    it 'climbs successfully when roll + strength is more than difficulty'
+    it 'fails climbing check when roll + strength is less than difficulty'
+
+
+  end
+end
