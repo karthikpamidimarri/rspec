6ade05d82cb84aa368a9afaa145d2abc082693c3
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-20 13:45:29 +0530

    Step 1.2: Implemented example climbs successfully when roll role strength is more than difficulty
    runs: bundle exec rspec game.rb
    
    Failures:
    
      1) Character climbing check skill climbs successfully when roll + strength is more than difficulty
         Failure/Error: let(:character) { Character.new(strength:5, die: die)}
    
         ArgumentError:
           wrong number of arguments (given 1, expected 0)

--- game.rb
+++ game.rb
@@ -4,8 +4,11 @@ end
 
 describe Character do
   describe 'climbing check skill' do
-    it 'climbs successfully when roll + strength is more than difficulty' do
+    let(:die){double} # In rspec we create double with simple double
+    let(:character) { Character.new(strength:5, die: die)}
 
+    it 'climbs successfully when roll + strength is more than difficulty' do
+      expect(character.climb(difficulty: 15)).to be_truthy
     end
     it 'fails climbing check when roll + strength is less than difficulty'
 
