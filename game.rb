7c5562070eb99ab4deef45af9a107c13c96841d3
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-20 13:23:32 +0530

    Step 1.1: Initialize the Character class

--- game.rb
+++ game.rb
@@ -1,6 +1,12 @@
+class Character
+
+end
+
 describe Character do
   describe 'climbing check skill' do
-    it 'climbs successfully when roll + strength is more than difficulty'
+    it 'climbs successfully when roll + strength is more than difficulty' do
+
+    end
     it 'fails climbing check when roll + strength is less than difficulty'
 
 
